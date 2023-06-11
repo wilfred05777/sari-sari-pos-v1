e4e6b3222a1cb57d0e8becd50e090c81ed502b67
Wilfred <wilfred05777@gmail.com>
2023-06-12 05:20:52 +0800

    move restructure files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// @ts-nocheck
+// import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
 
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  text-transform: uppercase;
+  /* font-style: oblique; */
+`
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+
+  margin: 20px;
+`
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+
+const StyledApp = styled.main`
+  background-color: red;
+  padding: 20px;
+`
+// const Spacing = styled.spacing`
+//   margin: 0 20px;
+// `
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles>
+        <StyledApp>
+          <H1>The Wild Oasis!</H1>
+          <Button onClick={() => alert('Check in')}>Check in</Button>
+
+          <Button onClick={() => alert('Check out')}>Check out</Button>
+
+          <Input type='number' placeholder='Number of guests' />
+          <Input type='number' placeholder='Number of guests' />
+        </StyledApp>
+      </GlobalStyles>
     </>
   )
 }
